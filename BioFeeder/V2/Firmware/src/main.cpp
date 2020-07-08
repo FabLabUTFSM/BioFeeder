5b6cca513b585744f868046ad4e6f5ac6161538d
jtomasd <josetodf@gmail.com>
2020-07-08 00:51:48 -0400

    3d printer buffer contruction

--- BioFeeder/V2/Firmware/src/main.cpp
+++ BioFeeder/V2/Firmware/src/main.cpp
@@ -5,14 +5,14 @@
 
 //OLed variables
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels4
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
 
 //I2C comunication
-int keyValue=0; //Key value we are looking to modify in the feeder
+float keyValue=0; //Key value we are looking to modify in the feeder
 bool i2c;
 int i2cRecieve1;
-String i2cDecoder[] = {"Neumatic Extruder" ,"Stepper Extruder"};
+String i2cDecoder[] = {"Neumatic Extruder" ,"Stepper Extruder", "-1" ,"1"};
 #define adress 99
 int buf[200];
 
@@ -22,28 +22,70 @@ int buf[200];
 #define SW 4
 #define botonPulse 250
 
-bool unitState = false;
+bool keyValueState = false;
 int lastStateCLK;
 int lastStateSW;
 int currentStateCLK;
 
 
+float power(float x, int y)
+{ 
+  float mult=1.0;
+  if (y>0)
+  {
+    for (int i=0 ; i < y ; i++ )
+    {
+      mult=mult*x;
+    }
+  }
+  else
+  {
+    for (int i=0 ; i < abs(y) ; i++ )
+    {
+      mult=mult/x;
+      Serial.println(mult);
+    }
+  return mult;
+}
+
+  
+  return mult;
+}
+void getKeyValue()
+{
+  if (i2cDecoder[buf[0]]=="Neumatic Extruder")
+  {
+    keyValue=  power(10,(buf[1]*i2cDecoder[buf[2]].toInt()))+255* buf[3]+buf[4]*i2cDecoder[buf[5]].toInt();
+  }
+  else
+  {
+    keyValue = power(10,(buf[1]*i2cDecoder[buf[2]].toInt()))*(255* buf[3]+buf[4]*i2cDecoder[buf[5]].toInt());
+    //keyValue = power(10,(buf[1]*i2cDecoder[buf[2]].toInt()));
+    Serial.println(keyValue);
+  }
+}
+
 void receiveEvent(int howMany)
 {
-  int pos=0;
+  unsigned int pos=0;
   while (Wire.available () > 0)
   {
    byte c = Wire.read ();
+   //Serial.println(c);
    if (pos < sizeof buf)
      buf [pos++] = c;
   } 
+  getKeyValue();
   i2c=true;
   return;
 }
 
+
 void flag ()
 {
- currentStateCLK = digitalRead(CLK);
+  if (keyValueState)
+  {
+    currentStateCLK = digitalRead(CLK);
     if (currentStateCLK != lastStateCLK  && currentStateCLK == 1){
       if (digitalRead(DT) != currentStateCLK) {
 			  keyValue --;
@@ -53,23 +95,23 @@ void flag ()
 		  }
 	  }
     lastStateCLK = currentStateCLK;
-  if (digitalRead(SW)==!lastStateSW)
+  /*if (digitalRead(SW)==!lastStateSW)
     {
     unitState= false;
     delay(botonPulse);
-    }
+    }*/
+  }
   return;
 }
 
 void setup() {
   Serial.begin(250000);
-
   //I2C initialization
   Wire.begin(adress); 
   Wire.onReceive(receiveEvent); // register event
 
   //Oled initialization
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
+  /*if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
     Serial.println(F("SSD1306 allocation failed"));
     for(;;);
   }
@@ -83,7 +125,7 @@ void setup() {
   display.display();
   delay(100);
   display.clearDisplay();
-  display.display();
+  display.display();*/
 
   //Encoder configuration
   pinMode(CLK,INPUT);
@@ -95,32 +137,35 @@ void setup() {
 }
 
 void loop() { 
+  //Serial.println(unitState);
   if (i2c){
     display.print(i2cDecoder[buf[0]]);
     display.display();
-    Serial.println("entre");
+    Serial.println(i2cDecoder[buf[0]]);
     i2c=false;
   }
 
   //Mostar en pantalla
   if (digitalRead(SW)==!lastStateSW){
-    unitState= true;
+    keyValueState= true;
     delay(botonPulse);
-    while (unitState)
+    while (keyValueState)
     {
-
-    if (digitalRead(SW)==!lastStateSW)
-      {
-      unitState= false;
-      delay(botonPulse);
+      Serial.println("entre");
+      if (digitalRead(SW)==!lastStateSW)
+        {
+        keyValueState= false;
+        delay(botonPulse);
       }
       display.clearDisplay();
       display.setCursor(0, 35);
       display.print("Key Value:");
       display.print(keyValue);
       display.display();
-      Serial.println(keyValue);
+      /*Serial.print("Key Value: ");
+      Serial.println(keyValue);*/
   }
+  Serial.println(keyValueState);
 }
 }
 
